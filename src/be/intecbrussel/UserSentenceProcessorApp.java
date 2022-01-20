2cb8e9abf1e544387f0dfda6e901006b6bf14580
erikholl <erik.hollanders@student.intecbrussel.be>
2022-01-20 10:41:07 +0100

    Updated and completed

--- src/be/intecbrussel/UserSentenceProcessorApp.java
+++ src/be/intecbrussel/UserSentenceProcessorApp.java
@@ -4,8 +4,6 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class UserSentenceProcessorApp {
@@ -20,6 +18,9 @@ public class UserSentenceProcessorApp {
 
         while (userInput.hasNextLine() && !((sentence = userInput.nextLine()).equals(
                 "exit"))) {
+            if (sentence.trim().isEmpty()) {
+                continue;
+            }
             writeToFile(path, sentence);
         }
     }
@@ -29,28 +30,40 @@ public class UserSentenceProcessorApp {
             Files.createDirectories(path.getParent());
             if (Files.notExists(path)) {
                 Files.createFile(path);
+            } else {
+                emptyFile(path);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void writeToFile(Path path, String input) {
-        try (FileWriter fileWriter = new FileWriter(path.toFile(), true);
-             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
-            bufferedWriter.write(input);
+    private static  void emptyFile(Path path) {
+        try (FileWriter fileWriter = new FileWriter(path.toFile())) {
+            fileWriter.write("");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-//    private static void readFromFile() {
-//        try (BufferedReader bufferedReader =
-//                     new BufferedReader(path.toFile()) ) {
-//
-//        }
-//
-//    }
+    private static String conversionText(String input) {
+        String lowerCase = input.toLowerCase();
+        String firstLetterCapital =
+                lowerCase.substring(0,1).toUpperCase().concat(lowerCase.substring(1));
+        if ( !(firstLetterCapital.endsWith(".") || firstLetterCapital.endsWith(
+                "?") || firstLetterCapital.endsWith("!")) ) {
+            firstLetterCapital = firstLetterCapital.concat(".");
+        }
+        return firstLetterCapital;
+    }
 
-
+    private static void writeToFile(Path path, String input) {
+        try (FileWriter fileWriter = new FileWriter(path.toFile(), true);
+             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+            input = conversionText(input);
+            bufferedWriter.write(input + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
